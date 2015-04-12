f8acaacfe8099dcedbc89adb51c4e0044f74ead8
西村真奈 <kabosudachi86@gmail.com>
2015-04-12 17:19:53 +0900

    first commit

--- swicount/ViewController.swift
+++ swicount/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label:UILabel!
+    var number:Int=0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func plus(){
+        number=number+1
+        label.text="\(number)"
+    }
+    @IBAction func minus(){
+        number=number-1
+        label.text="\(number)"
+    }
+    @IBAction func kakeru(){
+        number=number*2
+        label.text="\(number)"
+    }
+    @IBAction func waru(){
+        number=number/2
+        label.text="\(number)"
+    }
 
 }
 
